ad6ca7851ff651a1a92f5359e902b24ffa3231a9
Tawnee Harris <tawneeh@icloud.com>
2020-12-11 09:06:51 -0800

    add set up for first test

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class Bread
+  {
+    [TestMethod]
+    {
+
+    }
+  }
+}
