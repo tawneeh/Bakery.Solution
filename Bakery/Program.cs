28369ab935c7eca9a99877124a354149cdcf4dd0
Tawnee Harris <tawneeh@icloud.com>
2020-12-11 10:40:27 -0800

    add Program class

---
+++ Bakery/Program.cs
@@ -0,0 +1,24 @@
+using System;
+// using System.Collections.Generic;
+using Bakery.Models;
+
+namespace Bakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Wecome to Pierre's Bakery!");
+      Console.WriteLine("Would you like to order some delicious bread and pastries? If Yes [press 'Y'] otherwise [press Enter]");
+      string response = Console.ReadLine();
+      if (response == "Y" || response == "y")
+      {
+      Console.WriteLine("How many loaves of bread would you like?");
+      Bread newBread = new Bread(5, Console.ReadLine());
+      }
+
+    }
+
+  }
+
+}
