37beb4dc90106eb5fa025c5e6705ab8161179dd1
Tawnee Harris <tawneeh@icloud.com>
2020-12-11 14:01:18 -0800

    add Pastry class

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -1,7 +1,26 @@
+using System;
 using System.Collections.Generic;
-// namespace Bakery.Models
+namespace Bakery.Models
 
-// {
-//     public class Pastry
+{
+    public class Pastry
+    {
+        private int _price { get; set; }
+        public string PastryAmount { get; set; }
+        private static List<Pastry> _instances = new List<Pastry> {};
 
-// }
+        public Pastry(int price, string pastryAmount)
+        {
+            _price = 2;
+            PastryAmount = pastryAmount;
+            _instances.Add(this);
+        }
+        public int GetPastryTotalPrice()
+        {
+            PastryAmount = PastryAmount;
+            int pastries = Convert.ToInt32(PastryAmount);
+            return _price * pastries;
+        }
+
+    }
+}
