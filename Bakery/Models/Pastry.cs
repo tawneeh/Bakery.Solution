ba9cc15283ffd86c6c57919989487b66db44c8c8
Tawnee Harris <tawneeh@icloud.com>
2020-12-15 22:22:32 -0800

    remove unused method

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -1,25 +1,17 @@
-using System;
 using System.Collections.Generic;
 namespace Bakery.Models
 
 {
     public class Pastry
     {
-        private int _price { get; set; }
         public string PastryAmount { get; set; }
         private static List<Pastry> _instances = new List<Pastry> {};
 
         public Pastry(int price, string pastryAmount)
         {
-            _price = 2;
+            price = 2;
             PastryAmount = pastryAmount;
             _instances.Add(this);
-        }
-        public int GetPastryTotalPrice()
-        {
-            PastryAmount = PastryAmount;
-            int pastries = Convert.ToInt32(PastryAmount);
-            return _price * pastries;
         }
 
     }
