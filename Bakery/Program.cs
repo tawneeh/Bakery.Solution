f225b8b01614c38ad830841de7ae04daae1b8f2d
Tawnee Harris <tawneeh@icloud.com>
2020-12-15 22:23:19 -0800

    add breadAmount variable

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -14,8 +14,8 @@ namespace Bakery
       if (response == "Y" || response == "y")
       {
       Console.WriteLine("How many loaves of bread would you like?");
-      string loaves = Console.ReadLine();
-      int loavesInt = int.Parse(loaves);
+      string breadAmount = Console.ReadLine();
+      int loavesInt = int.Parse(breadAmount);
       Console.WriteLine("The Price is: $" + loavesInt * 5);
       }
       else
