64a9b386957bd5654a7221a23f28cdf4831464c3
Tawnee Harris <tawneeh@icloud.com>
2020-12-15 22:10:08 -0800

    add code to parse amount of bread input

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,5 +1,5 @@
 using System;
-// using System.Collections.Generic;
+using System.Collections.Generic;
 using Bakery.Models;
 
 namespace Bakery
@@ -14,10 +14,9 @@ namespace Bakery
       if (response == "Y" || response == "y")
       {
       Console.WriteLine("How many loaves of bread would you like?");
-      Bread newBread = new Bread(5, Console.ReadLine());
       string loaves = Console.ReadLine();
-      int loavesInt = Convert.ToInt32(loaves);
-      Console.WriteLine("Your total is $" + GetTotalBreadPrice(loavesInt));
+      int loavesInt = int.Parse(loaves);
+      Console.WriteLine("The Price is: $" + loavesInt * 5);
       }
       else
       {
