0c0a534b5d21a61167cf0ac8401694e2cd20a1d5
Tawnee Harris <tawneeh@icloud.com>
2020-12-15 22:22:04 -0800

    remove unused method

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,26 +1,18 @@
-using System;
 using System.Collections.Generic;
 namespace Bakery.Models
 
 {
     public class Bread
     {
-        private int _price { get; set; }
         public string BreadAmount { get; set; }
-        // private static List<Bread> _instances = new List<Bread> {};
+        private static List<Bread> _instances = new List<Bread> {};
 
         public Bread(int price, string breadAmount)
         {
-            _price = 5; // price = 5 * breadAmount as int!!
+            price = 5;
             BreadAmount = breadAmount;
-            // _instances.Add(this);
+            _instances.Add(this);
         }
 
-        public int GetBreadTotalPrice()
-        {
-            BreadAmount = BreadAmount;
-            int breadLoaves = Convert.ToInt32(BreadAmount);
-            return _price * breadLoaves;
-        }
     }
 }
